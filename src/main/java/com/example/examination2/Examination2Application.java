4462958bf4b414c8f254848ac7514d672d74ad3d
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-14 18:27:39 +0900

    first commit

---
+++ src/main/java/com/example/examination2/Examination2Application.java
@@ -0,0 +1,13 @@
+package com.example.examination2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examination2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examination2Application.class, args);
+	}
+
+}
