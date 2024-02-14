9d7ca43a2ff1c3d9ac865c5ed546139aecc24b2b
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-14 23:05:08 +0900

    sonarqubeの指摘によりExamination2ApplicationTestsにテストを作成。

--- src/test/java/com/example/examination2/Examination2ApplicationTests.java
+++ src/test/java/com/example/examination2/Examination2ApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.examination2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.examination2.presentation.controller.BookRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Examination2ApplicationTests {
 
+	@Autowired
+	private BookRestController bookRestController;
+
 	@Test
 	void contextLoads() {
+		assertThat(bookRestController).isNotNull();
 	}
 
 }
