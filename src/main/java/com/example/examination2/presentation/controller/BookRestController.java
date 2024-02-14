7925fdc50fef7f29a79eb101a62ef53f9f4116f3
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-14 23:01:36 +0900

    BookRestControllerの実装。

--- src/main/java/com/example/examination2/presentation/controller/BookRestController.java
+++ src/main/java/com/example/examination2/presentation/controller/BookRestController.java
@@ -1,5 +1,18 @@
 package com.example.examination2.presentation.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class BookRestController {
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void accessRoot() {
+        // 何もしない
+    }
 }
