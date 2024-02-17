e8a194142c75f1571c2e28a156665fb196d47828
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-17 19:40:41 +0900

    AllBooksResponseの実装。

--- src/main/java/com/example/examination2/presentation/response/AllBooksResponse.java
+++ src/main/java/com/example/examination2/presentation/response/AllBooksResponse.java
@@ -1,7 +1,10 @@
 package com.example.examination2.presentation.response;
 
+import com.example.examination2.domain.Book;
 import java.util.List;
 
 public record AllBooksResponse(List<BookResponse> books) {
-
+    public static AllBooksResponse createAllResponse(List<Book> bookList) {
+        return new AllBooksResponse(bookList.stream().map(BookResponse::createResponse).toList());
+    }
 }
