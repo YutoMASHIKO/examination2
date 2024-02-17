a42d8498631af69d7a105b0f7c5978d3a1d93de5
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-17 17:59:11 +0900

    BookにIDに関するガード条件の実装。

--- src/main/java/com/example/examination2/domain/Book.java
+++ src/main/java/com/example/examination2/domain/Book.java
@@ -1,5 +1,12 @@
 package com.example.examination2.domain;
 
+import static java.util.Objects.isNull;
+
 public record Book(String id, String title, String author, String publisher, Integer price) {
 
+    public Book {
+        if (isNull(id)) {
+            throw new IllegalArgumentException("本IDがnullです。");
+        }
+    }
 }
