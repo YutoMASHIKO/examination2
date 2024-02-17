5e1e1a74b74af18f6305c75c888cb280ef26a378
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-17 18:19:36 +0900

    BookTestのリファクタリング。テストパターンの追加。

--- src/test/java/com/example/examination2/domain/BookTest.java
+++ src/test/java/com/example/examination2/domain/BookTest.java
@@ -1,22 +1,24 @@
 package com.example.examination2.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BookTest {
 
-    @Test
-    void IDがnullの場合() {
-        assertThatThrownBy(() -> new Book(null, "テスト駆動開発", "Kent Beck", "オーム社", 3080))
+    @ParameterizedTest(name = "{2} の場合")
+    @CsvSource(delimiter = '|', textBlock = """
+           #         ID |             MESSAGE                | TESTNAME
+                        |                      IDがnullです。 | IDがnull
+                      a |               IDが数字ではありません。 | IDが数字以外
+                      0 | IDは1~9999999999でなくてはいけません。 | IDの数字が許容外
+            10000000000 | IDは1~9999999999でなくてはいけません。 | IDの数字が許容外
+           """)
+    void IDのガード条件に違反する場合(String id, String message, String testName) {
+        assertThatThrownBy(() -> new Book(id, "テスト駆動開発", "Kent Beck", "オーム社", 3080))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("IDがnullです。");
-    }
-
-    @Test
-    void IDが数字ではない場合() {
-        assertThatThrownBy(() -> new Book("a", "テスト駆動開発", "Kent Beck", "オーム社", 3080))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("IDが数字ではありません。");
+                .hasMessage(message);
     }
 }
