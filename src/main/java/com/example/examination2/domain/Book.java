f41b09bcbe40945328b4e164a1799dc323452ed4
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-17 18:04:50 +0900

    BookにIDに関するガード条件を追加実装。

--- src/main/java/com/example/examination2/domain/Book.java
+++ src/main/java/com/example/examination2/domain/Book.java
@@ -1,12 +1,16 @@
 package com.example.examination2.domain;
 
 import static java.util.Objects.isNull;
+import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 public record Book(String id, String title, String author, String publisher, Integer price) {
 
     public Book {
         if (isNull(id)) {
             throw new IllegalArgumentException("IDがnullです。");
+        }
+        if (!isNumeric(id)) {
+            throw new IllegalArgumentException("IDが数字ではありません。");
         }
     }
 }
