7bb5c6cc8749ce7ee97548473c8e156fa283669e
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-17 18:22:15 +0900

    Bookのガード条件の実装。

--- src/test/java/com/example/examination2/domain/BookTest.java
+++ src/test/java/com/example/examination2/domain/BookTest.java
@@ -1,6 +1,5 @@
 package com.example.examination2.domain;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
