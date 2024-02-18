f897c040d919125bd751f2ee30ea189cfc6c699b
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-18 23:11:00 +0900

    GetAllBooksUseCaseの実装。

--- src/main/java/com/example/examination2/application/GetAllBooksUseCase.java
+++ src/main/java/com/example/examination2/application/GetAllBooksUseCase.java
@@ -1,5 +1,17 @@
 package com.example.examination2.application;
 
-public class GetAllBooksUseCase {
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class GetAllBooksUseCase {
+    private final BookRepository bookRepository;
+
+    public List<Book> getAllBooks() {
+        return bookRepository.getAllBooks();
+    }
 }
