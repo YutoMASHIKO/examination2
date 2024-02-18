6b59e1e0a1c1269c8a13085e10012a1072b15eee
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-18 23:25:38 +0900

    BookRestControllerに全件表示を実装。

--- src/main/java/com/example/examination2/presentation/controller/BookRestController.java
+++ src/main/java/com/example/examination2/presentation/controller/BookRestController.java
@@ -1,5 +1,8 @@
 package com.example.examination2.presentation.controller;
 
+import com.example.examination2.application.GetAllBooksUseCase;
+import com.example.examination2.presentation.response.AllBooksResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class BookRestController {
+    private final GetAllBooksUseCase getAllBooksUseCase;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public void accessRoot() {
         // 何もしない
+    }
+
+    @GetMapping("v1/books")
+    @ResponseStatus(HttpStatus.OK)
+    public AllBooksResponse getAllBooks() {
+        return AllBooksResponse.createAllResponse(getAllBooksUseCase.getAllBooks());
     }
 }
