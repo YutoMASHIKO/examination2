4c04b302d6d5048a570190b91ee8b033400b94e9
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-18 22:17:17 +0900

    BookEntityの実装。

--- src/main/java/com/example/examination2/presentation/entity/BookEntity.java
+++ src/main/java/com/example/examination2/presentation/entity/BookEntity.java
@@ -1,5 +1,11 @@
 package com.example.examination2.presentation.entity;
 
-public class BookEntity {
+import com.example.examination2.domain.Book;
+
+public record BookEntity(String id, String title, String author, String publisher, Integer price) {
+
+    public Book convert() {
+        return new Book(id, title, author, publisher, price);
+    }
 
 }
