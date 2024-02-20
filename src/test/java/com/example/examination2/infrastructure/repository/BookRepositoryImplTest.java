a23d1d1db3c81a02dddbc83df77d97f49e6234a0
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-20 23:32:27 +0900

    BookRepositoryImplTestにIDによる個別検索のテストを追加。

--- src/test/java/com/example/examination2/infrastructure/repository/BookRepositoryImplTest.java
+++ src/test/java/com/example/examination2/infrastructure/repository/BookRepositoryImplTest.java
@@ -6,6 +6,7 @@ import com.example.examination2.infrastructure.mapper.BookMapper;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,37 +28,52 @@ class BookRepositoryImplTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void 全件取得をする場合() {
-        when(bookMapper.getAllBooks())
-                .thenReturn(
-                        List.of(
-                                new BookEntity("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-                                new BookEntity("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
-                                new BookEntity("3", "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
-                                new BookEntity("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)
-                        )
-                );
+    @Nested
+    class 全件取得 {
+        @Test
+        void 全件取得をする場合() {
+            when(bookMapper.getAllBooks())
+                    .thenReturn(
+                            List.of(
+                                    new BookEntity("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+                                    new BookEntity("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
+                                    new BookEntity("3", "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
+                                    new BookEntity("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)
+                            )
+                    );
 
-        List<Book> expected = List.of(
-                new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
-                new Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
-                new Book("3", "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
-                new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)
-        );
+            List<Book> expected = List.of(
+                    new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080),
+                    new Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860),
+                    new Book("3", "エクストリームプログラミング", "Kent Beck", "オーム社", 2420),
+                    new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)
+            );
 
-        List<Book> actual = sut.getAllBooks();
+            List<Book> actual = sut.getAllBooks();
 
-        assertEquals(expected, actual);
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void データが存在しないときに全件取得をする場合() {
+            when(bookMapper.getAllBooks()).thenReturn(new ArrayList<>());
+
+            List<Book> expected = new ArrayList<>();
+
+            List<Book> actual = sut.getAllBooks();
+
+            assertEquals(expected, actual);
+        }
     }
 
     @Test
-    void データが存在しないときに全件取得をする場合() {
-        when(bookMapper.getAllBooks()).thenReturn(new ArrayList<>());
+    void IDによる検索を行う場合() {
+        when(bookMapper.getBookById("1"))
+                .thenReturn(new BookEntity("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080));
 
-        List<Book> expected = new ArrayList<>();
+        Book expected = new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080);
 
-        List<Book> actual = sut.getAllBooks();
+        Book actual = sut.getBookById("1");
 
         assertEquals(expected, actual);
     }
