1befa11492ccee08ddb91e7b9560c5294e6cbc2f
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-20 23:55:35 +0900

    GetBookUseCaseの本の個別取得を実装。

--- src/main/java/com/example/examination2/application/GetBookUseCase.java
+++ src/main/java/com/example/examination2/application/GetBookUseCase.java
@@ -1,5 +1,18 @@
 package com.example.examination2.application;
 
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GetBookUseCase {
 
+    private final BookRepository bookRepository;
+
+    public Book getBookById(String id) {
+        return bookRepository.getBookById(id)
+                .orElseThrow();
+    }
 }
