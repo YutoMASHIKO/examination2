fdee891c7e5422e3788c4458dffb4fb6373110e9
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 00:20:00 +0900

    BookRestControllerTestを修正。

--- src/test/java/com/example/examination2/presentation/controller/BookRestControllerTest.java
+++ src/test/java/com/example/examination2/presentation/controller/BookRestControllerTest.java
@@ -10,6 +10,7 @@ import com.example.examination2.domain.Book;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -81,37 +82,40 @@ class BookRestControllerTest {
                 .body("books[3].price", equalTo(2640));
     }
 
-    @Test
-    void IDが1の本を取得できる場合() {
-        when(getBookUseCase.getBookById("1"))
-                .thenReturn(new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080));
+    @Nested
+    class ID検索 {
+        @Test
+        void IDが1の本を取得できる場合() {
+            when(getBookUseCase.getBookById("1"))
+                    .thenReturn(new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080));
 
-        given()
-                .when()
-                .get("/v1/books/1")
-                .then()
-                .status(HttpStatus.OK)
-                .body("id", equalTo("1"))
-                .body("title", equalTo("テスト駆動開発"))
-                .body("author", equalTo("Kent Beck"))
-                .body("publisher", equalTo("オーム社"))
-                .body("price", equalTo(3080));
-    }
+            given()
+                    .when()
+                    .get("/v1/books/1")
+                    .then()
+                    .status(HttpStatus.OK)
+                    .body("id", equalTo("1"))
+                    .body("title", equalTo("テスト駆動開発"))
+                    .body("author", equalTo("Kent Beck"))
+                    .body("publisher", equalTo("オーム社"))
+                    .body("price", equalTo(3080));
+        }
 
-    @Test
-    void IDが2の本を取得できる場合() {
-        when(getBookUseCase.getBookById("2"))
-                .thenReturn(new Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
+        @Test
+        void IDが2の本を取得できる場合() {
+            when(getBookUseCase.getBookById("2"))
+                    .thenReturn(new Book("2", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860));
 
-        given()
-                .when()
-                .get("/v1/books/2")
-                .then()
-                .status(HttpStatus.OK)
-                .body("id", equalTo("2"))
-                .body("title", equalTo("アジャイルサムライ"))
-                .body("author", equalTo("Jonathan Rasmusson"))
-                .body("publisher", equalTo("オーム社"))
-                .body("price", equalTo(2860));
+            given()
+                    .when()
+                    .get("/v1/books/2")
+                    .then()
+                    .status(HttpStatus.OK)
+                    .body("id", equalTo("2"))
+                    .body("title", equalTo("アジャイルサムライ"))
+                    .body("author", equalTo("Jonathan Rasmusson"))
+                    .body("publisher", equalTo("オーム社"))
+                    .body("price", equalTo(2860));
+        }
     }
 }
