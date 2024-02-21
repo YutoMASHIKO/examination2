c228fcc6227617206fc612bf71fc011c0da2d408
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 23:55:36 +0900

    InsertBookUseCaseの修正。

--- src/main/java/com/example/examination2/application/InsertBookUseCase.java
+++ src/main/java/com/example/examination2/application/InsertBookUseCase.java
@@ -1,5 +1,6 @@
 package com.example.examination2.application;
 
+import com.example.examination2.application.data.InsertBook;
 import com.example.examination2.domain.Book;
 import com.example.examination2.domain.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class InsertBookUseCase {
     private final BookRepository bookRepository;
 
-    public Book insertBook(Book book) {
-        return bookRepository.insertBook(book);
+    public Book insertBook(InsertBook insertBook) {
+        return bookRepository.insertBook(insertBook.convert(bookRepository.getNextId()));
     }
 }
--- src/test/java/com/example/examination2/application/InsertBookUseCaseTest.java
+++ src/test/java/com/example/examination2/application/InsertBookUseCaseTest.java
@@ -3,6 +3,7 @@ package com.example.examination2.application;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+import com.example.examination2.application.data.InsertBook;
 import com.example.examination2.domain.Book;
 import com.example.examination2.domain.repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -25,14 +26,14 @@ class InsertBookUseCaseTest {
 
     @Test
     void 従業員の新規登録をする場合() {
-        when(bookRepository.getNextId()).thenReturn(3L);
+        when(bookRepository.getNextId()).thenReturn(4L);
 
         when(bookRepository.insertBook(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)))
                 .thenReturn(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
 
         Book expected = new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640);
 
-        Book actual = sut.insertBook(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
+        Book actual = sut.insertBook(new InsertBook( "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
 
         assertEquals(expected, actual);
     }
