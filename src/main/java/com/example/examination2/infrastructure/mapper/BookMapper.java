4985ae2d541b5b5b601545d449efbe8d9edd5926
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 20:34:43 +0900

    BookMapperに新規登録を実装。

--- src/main/java/com/example/examination2/infrastructure/mapper/BookMapper.java
+++ src/main/java/com/example/examination2/infrastructure/mapper/BookMapper.java
@@ -2,6 +2,7 @@ package com.example.examination2.infrastructure.mapper;
 
 import com.example.examination2.infrastructure.entity.BookEntity;
 import java.util.List;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
@@ -18,4 +19,7 @@ public interface BookMapper {
     @Select("SELECT nextval('BOOK_ID_SEQ')")
     @Options(flushCache = Options.FlushCachePolicy.TRUE)
     Long getNextId();
+
+    @Insert("INSERT INTO books (id, title, author, publisher, price) VALUES(#{id}, #{title}, #{author}, #{publisher}, #{price})")
+    Integer insert(BookEntity bookEntity);
 }
