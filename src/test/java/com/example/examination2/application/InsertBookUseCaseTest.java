cadef3553bb25127740497d9bba351f12c144979
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 21:25:34 +0900

    InsertBookUseCaseの仮実装。 InsertBookUseCaseTestの作成。

---
+++ src/test/java/com/example/examination2/application/InsertBookUseCaseTest.java
@@ -0,0 +1,39 @@
+package com.example.examination2.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class InsertBookUseCaseTest {
+    @InjectMocks
+    InsertBookUseCase sut;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void 従業員の新規登録をする場合() {
+        when(bookRepository.getNextId()).thenReturn(3L);
+
+        when(bookRepository.insertBook(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640)))
+                .thenReturn(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
+
+        Book expected = new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640);
+
+        Book actual = sut.insertBook(new Book("4", "Clean Agile", "Robert C. Martin", "ドワンゴ", 2640));
+
+        assertEquals(expected, actual);
+    }
+}
