76efaab66acf260296c384ac9bf5498d5976d8fb
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 21:27:50 +0900

    InsertBookUseCaseの実装。

--- src/main/java/com/example/examination2/application/InsertBookUseCase.java
+++ src/main/java/com/example/examination2/application/InsertBookUseCase.java
@@ -1,5 +1,16 @@
 package com.example.examination2.application;
 
-public class InsertBookUseCase {
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class InsertBookUseCase {
+    private final BookRepository bookRepository;
+
+    public Book insertBook(Book book) {
+        return bookRepository.insertBook(book);
+    }
 }
