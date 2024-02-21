5ce0950ac9f8e27572dd0971efa8957cf28b64e9
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-21 23:51:44 +0900

    InsertBook実装。

--- src/main/java/com/example/examination2/application/data/InsertBook.java
+++ src/main/java/com/example/examination2/application/data/InsertBook.java
@@ -1,5 +1,10 @@
 package com.example.examination2.application.data;
 
+import com.example.examination2.domain.Book;
+
 public record InsertBook(String title, String author, String publisher, Integer price) {
 
+    public Book convert(Long id) {
+        return new Book(String.valueOf(id), title, author, publisher, price);
+    }
 }
