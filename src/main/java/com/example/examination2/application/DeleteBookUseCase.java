a6b3d2f04253e8fd0910fe18c92c101864db3646
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 22:54:21 +0900

    DeleteBookUseCaseの実装。

--- src/main/java/com/example/examination2/application/DeleteBookUseCase.java
+++ src/main/java/com/example/examination2/application/DeleteBookUseCase.java
@@ -1,5 +1,15 @@
 package com.example.examination2.application;
 
-public class DeleteBookUseCase {
+import com.example.examination2.domain.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+public class DeleteBookUseCase {
+    private final BookRepository bookRepository;
+
+    public void deleteBook(String id) {
+        bookRepository.deleteBook(id);
+    }
 }
