d346eb8880d4657433c116f2dcaed7d3b2e942f8
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 20:50:32 +0900

    UpdateBookDataTestの修正。

--- src/test/java/com/example/examination2/application/data/UpdateBookDataTest.java
+++ src/test/java/com/example/examination2/application/data/UpdateBookDataTest.java
@@ -3,20 +3,33 @@ package com.example.examination2.application.data;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.examination2.domain.Book;
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class UpdateBookDataTest {
 
-    @Test
-    void すべての要素を更新する場合() {
-        UpdateBookData sut = new UpdateBookData("1", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860);
+    @ParameterizedTest
+    @MethodSource("insertData")
+    void 更新用のモデルに変換する場合(UpdateBookData sut, Book expected) {
         Book originalBook = new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080);
-
-        Book expected = new Book("1", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860);
 
         Book actual = sut.convert(originalBook);
 
         assertEquals(expected, actual);
     }
 
+    private static Stream<Arguments> insertData() {
+        return Stream.of(
+                Arguments.of(
+                        new UpdateBookData("1", "アジャイルサムライ", "Jonathan Rasmusson", null, 2860),
+                        new Book("1", "アジャイルサムライ", "Jonathan Rasmusson", "オーム社", 2860)
+                ),
+                Arguments.of(
+                        new UpdateBookData("1", null, null, "ドワンゴ", null),
+                        new Book("1", "テスト駆動開発", "Kent Beck", "ドワンゴ", 3080)
+                )
+        );
+    }
 }
