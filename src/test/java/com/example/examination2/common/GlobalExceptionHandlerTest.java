adaee43fc1643a7cfbcf01dc0679c2bf0e5285d7
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-25 00:18:29 +0900

    その他の例外処理のテストを追加。

--- src/test/java/com/example/examination2/common/GlobalExceptionHandlerTest.java
+++ src/test/java/com/example/examination2/common/GlobalExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package com.example.examination2.common;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static java.util.Collections.emptyList;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.when;
 
@@ -71,6 +72,22 @@ class GlobalExceptionHandlerTest {
                 .patch("/v1/books/1")
                 .then()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("code", equalTo("0004"));
+                .body("code", equalTo("0004"))
+                .body("message", equalTo("SQLの実行に失敗しました"))
+                .body("details", equalTo(emptyList()));
+    }
+
+    @Test
+    void その他予期しない例外が発生した場合() {
+        when(getAllBooksUseCase.getAllBooks()).thenThrow(RuntimeException.class);
+
+        given()
+                .when()
+                .get("/v1/books")
+                .then()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("code", equalTo("9999"))
+                .body("message", equalTo("An unexpected exception occurred: null"))
+                .body("details", equalTo(emptyList()));
     }
 }
