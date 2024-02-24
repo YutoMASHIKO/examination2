ec25ac047d479a8daab7e552d4316f5e7526f65a
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 19:47:58 +0900

    UpdateBookDataの実装。

--- src/main/java/com/example/examination2/application/data/UpdateBookData.java
+++ src/main/java/com/example/examination2/application/data/UpdateBookData.java
@@ -1,5 +1,32 @@
 package com.example.examination2.application.data;
 
+import static java.util.Objects.nonNull;
+
+import com.example.examination2.domain.Book;
+
 public record UpdateBookData(String id, String title, String author, String publisher, Integer price)  {
 
+    public Book convert(Book book) {
+        String updatedTitle = book.title();
+        if (nonNull(title)) {
+            updatedTitle = title;
+        }
+
+        String updatedAuthor = book.author();
+        if (nonNull(author)) {
+            updatedAuthor = author;
+        }
+
+        String updatedPublisher = book.publisher();
+        if (nonNull(publisher)) {
+            updatedPublisher = publisher;
+        }
+
+        Integer updatedPrice = book.price();
+        if (nonNull(price)) {
+            updatedPrice = price;
+        }
+
+        return new Book(book.id(), updatedTitle, updatedAuthor, updatedPublisher, updatedPrice);
+    }
 }
