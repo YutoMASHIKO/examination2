0ef2b565be5ef6847c4802b7ee6b76d6c8b7beec
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 22:53:54 +0900

    DeleteBookUseCaseの仮実装。 DeleteBookUseCaseTestの作成。

---
+++ src/test/java/com/example/examination2/application/DeleteBookUseCaseTest.java
@@ -0,0 +1,32 @@
+package com.example.examination2.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class DeleteBookUseCaseTest {
+    @InjectMocks
+    DeleteBookUseCase sut;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void 削除を行う場合() {
+        assertDoesNotThrow(() -> sut.deleteBook("1"));
+    }
+
+}
