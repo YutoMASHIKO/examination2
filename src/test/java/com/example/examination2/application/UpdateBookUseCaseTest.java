f0f781fe3f4d1ad201eeb284421e5af9a2b39b68
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 20:03:04 +0900

    UpdateBookUseCaseの仮実装。 UpdateBookUseCaseTestの作成。

---
+++ src/test/java/com/example/examination2/application/UpdateBookUseCaseTest.java
@@ -0,0 +1,38 @@
+package com.example.examination2.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import com.example.examination2.application.data.UpdateBookData;
+import com.example.examination2.domain.Book;
+import com.example.examination2.domain.repository.BookRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class UpdateBookUseCaseTest {
+
+    @InjectMocks
+    UpdateBookUseCase sut;
+
+    @Mock
+    BookRepository bookRepository;
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void 本の更新を行う場合() {
+        when(bookRepository.getBookById("1"))
+                .thenReturn(Optional.of(new Book("1", "テスト駆動開発", "Kent Beck", "オーム社", 3080)));
+
+        UpdateBookData updateBookData = new UpdateBookData("1", "テスト駆動開発", "Uncle Bob", "オーム社", 3080);
+
+        assertDoesNotThrow(() -> sut.updateBook(updateBookData));
+    }
+}
