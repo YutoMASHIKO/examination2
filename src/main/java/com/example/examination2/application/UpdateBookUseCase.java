b93385729033573019228e8a05816e65d15ed808
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 20:05:46 +0900

    UpdateBookUseCaseの実装。

--- src/main/java/com/example/examination2/application/UpdateBookUseCase.java
+++ src/main/java/com/example/examination2/application/UpdateBookUseCase.java
@@ -1,5 +1,21 @@
 package com.example.examination2.application;
 
+import com.example.examination2.application.data.UpdateBookData;
+import com.example.examination2.domain.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateBookUseCase {
+    private final BookRepository bookRepository;
+
+    public void updateBook(UpdateBookData updateBookData) {
+        bookRepository.updateBook(
+                updateBookData.convert(
+                        bookRepository.getBookById(updateBookData.id())
+                                .orElseThrow(RuntimeException::new))
+                );
+    }
 
 }
