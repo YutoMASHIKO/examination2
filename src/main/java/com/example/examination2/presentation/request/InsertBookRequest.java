3a5d0be4c389d9c4dec992ab6a0ebbd9d9a75add
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 17:40:56 +0900

    タイトル以外のパラメータについてもvalidationを付与。

--- src/main/java/com/example/examination2/presentation/request/InsertBookRequest.java
+++ src/main/java/com/example/examination2/presentation/request/InsertBookRequest.java
@@ -1,7 +1,9 @@
 package com.example.examination2.presentation.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 /**
@@ -17,8 +19,20 @@ public record InsertBookRequest(
         @Length(max = 100)
         @JsonProperty("title")
         String title,
+
+        @NotBlank
+        @Length(max = 100)
+        @JsonProperty("author")
         String author,
+
+        @NotBlank
+        @Length(max = 100)
+        @JsonProperty("publisher")
         String publisher,
+
+        @NotNull
+        @Min(1)
+        @JsonProperty("price")
         Integer price
 )  {
 
