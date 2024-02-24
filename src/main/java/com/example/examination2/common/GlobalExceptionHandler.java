8051d41e5b031b2bf59c660451abcc9d3954ea1c
Yuto MASHIKO <mashiko_yuto@ims-sol.co.jp>
2024-02-24 17:11:23 +0900

    新規登録においてtitleに関するvalidationを追加。

--- src/main/java/com/example/examination2/common/GlobalExceptionHandler.java
+++ src/main/java/com/example/examination2/common/GlobalExceptionHandler.java
@@ -4,8 +4,12 @@ import static java.util.Collections.emptyList;
 
 import com.example.examination2.application.exception.BookNotFoundException;
 import com.example.examination2.presentation.response.ErrorResponse;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -29,6 +33,27 @@ public class GlobalExceptionHandler {
                 "0003",
                 String.format("specified book [id = %s] is not found.", e.getId()),
                 emptyList()
+        );
+    }
+
+    /**
+     * リクエストの入力に問題があった場合の例外ハンドリングメソッドです.
+     *
+     * @param e リクエストの入力に問題があった場合に発生する例外
+     * @return バリデーションエラーに関する詳細情報を含むエラーレスポンス
+     */
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        List<String> details = new ArrayList<>();
+
+        for (FieldError error : e.getFieldErrors()) {
+            details.add(String.format("%s %s", error.getField(), error.getDefaultMessage()));
+        }
+        return new ErrorResponse(
+                "0002",
+                "request validation error is occurred.",
+                details
         );
     }
 }
--- src/main/java/com/example/examination2/presentation/request/InsertBookRequest.java
+++ src/main/java/com/example/examination2/presentation/request/InsertBookRequest.java
@@ -1,5 +1,25 @@
 package com.example.examination2.presentation.request;
 
-public record InsertBookRequest(String title, String author, String publisher, Integer price)  {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+/**
+ * 新しい本データを作成するために送信するリクエストを表すクラスです.
+ *
+ * @param title     本のタイトル
+ * @param author    本の著者
+ * @param publisher 本の出版社
+ * @param price     本の価格
+ */
+public record InsertBookRequest(
+        @NotBlank
+        @Length(max = 100)
+        @JsonProperty("title")
+        String title,
+        String author,
+        String publisher,
+        Integer price
+)  {
 
 }
